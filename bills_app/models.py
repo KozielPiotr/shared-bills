36e5db709e3cf7a314cc2ca5b037a0b192ffd2d6
Cezary Miącz <czarek.miacz@gmail.com>
2019-12-09 22:50:09 +0100

    Apply black formatting

--- bills_app/models.py
+++ bills_app/models.py
@@ -15,9 +15,15 @@ class CustomUser(AbstractUser):
 
 class Event(models.Model):
     """Settlement for whole event, eg. kayaks."""
+
     name = models.CharField(max_length=500, unique=True)
-    cashier = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.SET_NULL,
-                                related_name="cashier")
+    cashier = models.ForeignKey(
+        CustomUser,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="cashier",
+    )
     participators = models.ManyToManyField(CustomUser, related_name="participates")
 
     def __str__(self):
@@ -26,11 +32,17 @@ class Event(models.Model):
 
 class Receipt(models.Model):
     """Single bill, eg. for car, food, fuel etc."""
+
     for_what = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     event = models.ForeignKey(Event, on_delete=models.PROTECT)
-    paid_by = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.PROTECT,
-                                related_name="paid_for")
+    paid_by = models.ForeignKey(
+        CustomUser,
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        related_name="paid_for",
+    )
     bought_by = models.ManyToManyField(CustomUser, related_name="bought")
 
     def __str__(self):
@@ -39,8 +51,13 @@ class Receipt(models.Model):
 
 class Payment(models.Model):
     """Payment from one to another."""
-    who_pays = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="paid_cash")
-    for_who = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="got_cash")
+
+    who_pays = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="paid_cash"
+    )
+    for_who = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="got_cash"
+    )
     event = models.ForeignKey(Event, on_delete=models.PROTECT)
 
     def __str__(self):
