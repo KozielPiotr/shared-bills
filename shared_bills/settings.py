45d61f79c894f8cffcaab6bda319f241328edbbc
KozielPiotr <koziel.piotr1988@gmail.com>
2020-02-17 00:15:05 +0100

    Settings fixed

--- shared_bills/settings.py
+++ shared_bills/settings.py
@@ -169,7 +169,7 @@ SIMPLE_JWT = {
     "ISSUER": None,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "email",
-    "USER_ID_CLAIM": "user_id",
+    "USER_ID_CLAIM": "email",
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
     "JTI_CLAIM": "jti",
